c2b409a6d405eb4402b447edfb5732fae96045dd
Adiel Dantas <adieldantasn@gmail.com>
2025-08-05 15:17:39 -0300

    Projeto iniciado

---
+++ src/test/java/br/com/reservahotel/reserva_hotel/ReservaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reservahotel.reserva_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
