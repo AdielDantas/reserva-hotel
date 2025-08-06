b226dea9c28cb86bfe767fddcb90af385e1a836d
Adiel Dantas <adieldantasn@gmail.com>
2025-08-06 14:49:18 -0300

    endpoint de buscar usuáro por Id

---
+++ src/main/java/br/com/reservahotel/reserva_hotel/controllers/UsuarioController.java
@@ -0,0 +1,24 @@
+package br.com.reservahotel.reserva_hotel.controllers;
+
+import br.com.reservahotel.reserva_hotel.model.dto.UsuarioDTO;
+import br.com.reservahotel.reserva_hotel.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDTO> buscarPorId(@PathVariable Long id) {
+        UsuarioDTO usuarioDTO = service.buscarPorId(id);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+}
